6e3d01448b7ae7e5269fe41ff098ba1437c8379a
Prince(Android) <malaniprince9@gmail.com>
2025-06-01 10:21:17 +0530

    First Commit

---
+++ app/src/main/java/com/example/quickbuy/SecondFragment.kt
@@ -0,0 +1,40 @@
+package com.example.quickbuy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.quickbuy.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textForDisplay = arguments?.getString("data").toString()
+        binding.textviewSecond.text = textForDisplay
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
