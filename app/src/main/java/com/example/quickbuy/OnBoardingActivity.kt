3455c6eaba68e27cc2e6b4d0bc8de3f2ba34d7b1
Prince(Android) <malaniprince9@gmail.com>
2025-06-10 00:58:11 +0530

    Changing in splash screen and onboarding screen

--- app/src/main/java/com/example/quickbuy/OnBoardingActivity.kt
+++ app/src/main/java/com/example/quickbuy/OnBoardingActivity.kt
@@ -8,18 +8,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quickbuy.databinding.ActivityOnBoardingBinding
 
+class OnBoardingActivity : AppCompatActivity() {
 
-class SplashActivity : AppCompatActivity() {
-    private val splashTime: Long = 2000
+    private lateinit var binding: ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@SplashActivity, OnBoardingActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, splashTime)
+      binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       binding.OnboardingButton.setOnClickListener {
+           startActivity(Intent(this,MainActivity::class.java))
+       }
     }
 }
